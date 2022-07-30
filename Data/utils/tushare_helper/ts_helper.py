7551edebbe2d0284b6056822927b55e198b6e558
ShiboXing <shibox@amazon.com>
2022-07-30 17:26:20 -0400

    feat: add get stock price, add exchange code into stock data

--- Data/utils/tushare_helper/ts_helper.py
+++ Data/utils/tushare_helper/ts_helper.py
@@ -17,16 +17,28 @@ class ts_helper:
     def get_stock_list(self):
 
         code_col_name = "成分券代码Constituent Code"
+        ex_col_name = "交易所英文名称Exchange(Eng)"
         hs = pd.read_excel(_hs300_url)
         zz = pd.read_excel(_zz500_url)
-        stock_list = pd.concat((hs[code_col_name], zz[code_col_name]), axis=0)
+        stock_list = pd.concat(
+            (hs[[code_col_name, ex_col_name]], zz[[code_col_name, ex_col_name]]), axis=0)
         stock_list = stock_list.astype({code_col_name: 'str'})
 
         for i in range(len(stock_list)):
-            code_len = len(stock_list.iloc[i])
+            code_len = len(stock_list.iloc[i, 0])
+            # add back the missing leading 0s
             if code_len < 6:
-                stock_list.iloc[i] = '0' * (6-code_len) + stock_list.iloc[i]
-        
+                stock_list.iloc[i, 0] = '0' * \
+                    (6-code_len) + stock_list.iloc[i, 0]
+            
+            # add exchange code into stock code
+            stock_ex = stock_list.iloc[i, 1].lower()
+            if 'shenzhen' in stock_ex:
+                stock_list.iloc[i, 0] += '.SZ'
+            if 'shanghai' in stock_ex:
+                stock_list.iloc[i, 0] += '.SH'
+
+        stock_list = stock_list[[code_col_name]]
         return stock_list
 
     def get_stock_price_daily(codes: list, N):
@@ -34,12 +46,20 @@ class ts_helper:
         get the daily bars of a stock starting from today to N days back
         '''
 
-        end_date = date.today()
-        start_date = end_date - timedelta(days=N)
+        df = pd.DataFrame()
+        window = 50
+        for i in range(0, len(codes), window):
+            end = max(i+window+1, len(codes))
+            part_codes = ",".join(codes[i:end])
+            end_date = date.today()
+            start_date = end_date - timedelta(days=N)
 
-        end_date_str = end_date.strftime("%Y%m%d")
-        start_date_str = start_date.strftime("%Y%m%d")
+            end_date_str = end_date.strftime("%Y%m%d")
+            start_date_str = start_date.strftime("%Y%m%d")
 
-        df = _pro_ts.daily(
-            ts_code=codes, start_date=start_date_str, end_dat=end_date_str)
+            tmp = _pro_ts.daily(
+                ts_code=part_codes, start_date=start_date_str, end_dat=end_date_str)
+
+            df = pd.concat((df, tmp), axis=0)
+
         return df
