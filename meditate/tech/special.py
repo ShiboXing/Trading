c0b2f61b288cf4c1d256c6d2ae66fced6f3813a2
Shibo Xing <shibox@amazon.com>
2022-08-17 01:06:09 -0400

    feat: add day streak signal filter

--- meditate/tech/special.py
+++ meditate/tech/special.py
@@ -1,64 +1,81 @@
 from pandas import DataFrame as DF
 from collections import deque
+from operator import lt, gt
 
 import pandas as pd
 
 
-def make_day_streak(days: int, is_up_streak: False):
+def make_day_streak(days: int, is_up_streak=False):
 
     # create column for each day's feature
     cols = {}
-    features = ["open_chg", "hi_chg", "lo_chg", "close_chg"]
-    for i in range(1, days + 1):
+    features = ["open", "high", "low", "close"]
+    for i in range(days):  # features * days
         for j in range(len(features)):
             cols = {**cols, f"{features[j]}{i}": []}
+    comp = gt if is_up_streak else lt
 
     def day_streak(df: DF):
-
-        signals = DF(cols)
-        feat_matrix = DF({f: [] for f in features})
-        """
-        TODO: revise signal filter logic
-        """
-
-        def compute_returns(window):
-            global feat_matrix
-
-            inds = window.index
-            tmp_feats = {f: [] for f in features}
-            cl0, op1, hi1, lo1, cl1 = (
-                df.loc[inds[0]].close,
-                df.loc[inds[1]].open,
-                df.loc[inds[1]].high,
-                df.loc[inds[1]].low,
-                df.loc[inds[1]].close,
-            )
-            tmp_feats["open_chg"] = [(op1 - cl0) / cl0]
-            tmp_feats["hi_chg"] = [(hi1 - op1) / op1]
-            tmp_feats["lo_chg"] = [(lo1 - op1) / op1]
-            tmp_feats["cl_chg"] = [(cl1 - op1) / op1]
-            # feat_matrix = pd.concat([feat_matrix, tmp_feats])
-
-            return 0
-
         df = df.sort_values(["ts_code", "trade_date"])
         df = df.reset_index().drop(
             columns=["index"]
         )  # reset index for better lookup runtime (unique and sorted)
-        df.rolling(window=2).apply(compute_returns)
 
         """
         use a finite state machine to determine streaks, 
         signal is valid is the final state is reached
         """
-        states = 0
-        # for i in range(1, days+1):
-        #     signals.append((
+        row = 0
+        state = 0
+        signals = DF(cols)
+        window = deque([])  # cache for finite state machine
 
-        #     ))
+        while row < len(df):
+            """
+            collect signals
+            """
+            if state == days - 1:
+                tmp_df = DF(cols)
+                for i in range(len(window)):
+                    for f in features:
+                        cols[f"{f}{i}"] = [window[i][f]]
+                cols["ts_code"] = df.loc[row].ts_code
+                cols["trade_date"] = df.loc[row].trade_date
+                signals = pd.concat((signals, tmp_df))
 
-        # for i in range(len(df)):
+            if not row or df.loc[row].ts_code != df.loc[row - 1].ts_code:
+                """
+                switch code, fill the initial window
+                """
+                window = deque([])
+                for i in range(days):
+                    window.append(df.loc[row + i][features])
+                    if len(window) > 1:
+                        state = (
+                            min(state + 1, days - 1)
+                            if comp(window[-1].close, window[-2].close)
+                            else 0
+                        )
+                row += 5
+            else:
+                """
+                shift the sliding window, load signal if valid
+                """
+                window.popleft()
+                window.append(df.loc[row][features])
+                state = (
+                    min(state + 1, days - 1)
+                    if comp(window[-1].close, window[-2].close)
+                    else 0
+                )
+                row += 1
 
-        #     df.iloc[i]
+            assert days - 1 >= state >= 0
+            
+        signals = signals.sort_values(["ts_code", "trade_date"])
+        signals = signals.reset_index().drop(
+            columns=["index"]
+        )  # reset index for better lookup runtime (unique and sorted)
+        return signals
 
     return day_streak
