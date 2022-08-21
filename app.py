813b372b26153dbfd7825f7f5ae972e9f2fb5fa4
Shibo Xing <shibox@amazon.com>
2022-08-21 01:16:35 -0400

    feat: add get streak in driver

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from detes import fetcher
+from meditate.tech.special import day_streak
+from detes.cache_helper import cache_helper as ch
 
-ft = fetcher(365)
-ft.update_quotes()
-ft.fetch_all_hist()
+if __name__ == "__main__":
+    ft = fetcher(400)
+    ft.update_quotes()
+    ft.fetch_all_hist()
+    
+    _ch = ch()
+    hist = _ch.load_data("hist")
+    sig5 = day_streak(hist, 5, False)
+
