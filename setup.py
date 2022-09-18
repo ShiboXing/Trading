77f0483d7331e3be9769d2e359b3de2d23036e6d
Shibo Xing <shibox@amazon.com>
2022-09-18 17:12:20 -0400

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, Extension, find_packages
+
+setup(
+    name="rumble",
+    version="1.0",
+    packages=find_packages("."),
+    # ext_modules=[
+    #     Extension(
+    #         "tacalc",
+    #         sources=[""],
+    #     )
+    # ],
+)
