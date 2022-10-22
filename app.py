8132109267e05bf2d0e1f6437854721a39d3cf46
Shibo Xing <shibox@amazon.com>
2022-10-22 19:13:53 -0400

    feat: fetch the entire streak from c++ since dataframe searching is slow

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
 import sys
 
-# sys.path.insert(
-#     0,
-#     "/Users/shibox/repos/Trading-Container/Trading/build/lib.macosx-12-x86_64-cpython-310/",
-# )
-# i, j = 0, len(sys.path) - 1
-# while i < j:
-#     if "Trading" in sys.path[i]:
-#         sys.path[i], sys.path[j] = sys.path[j], sys.path[i]
-#         i, j = i + 1, j - 1
-#     else:
-#         i += 1
-
 import time, os
 import numpy as np
 from rumble.detes import fetcher
@@ -44,4 +32,3 @@ if __name__ == "__main__":
     hist = hist.sort_values(by=["ts_code", "trade_date"])
     for key in obj:
         print(key)
-        # print(hist[(hist.ts_code == ts_code) & (hist.trade_date == date)])
