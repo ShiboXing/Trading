58bc04e70f6d46738d30e4ae8dc7104a22be6497
Shibo Xing <shx26@pitt.edu>
2022-10-30 19:44:57 -0400

    feat: add calendar schema

--- db/detes/stock.sql
+++ db/detes/stock.sql
@@ -2,8 +2,8 @@ USE master
 GO
 IF NOT EXISTS (
    SELECT name
-   FROM sys.databases
-   WHERE name = N'detes'
+FROM sys.databases
+WHERE name = N'detes'
 )
 CREATE DATABASE [detes]
 GO
@@ -11,7 +11,8 @@ GO
 use detes
 GO
 
-if not exists (select * from sys.tables 
+if not exists (select *
+from sys.tables
 where name='stocks')
 begin
   CREATE TABLE stocks
@@ -23,12 +24,13 @@ begin
 end
 GO
 
-if not exists (select * from sys.tables 
+if not exists (select *
+from sys.tables
 where name='daily_bars')
 BEGIN
   CREATE table daily_bars
   (
-    [exchange] nvarchar(5) not null,
+    [exchange] nvarchar(6) not null,
     [code] nvarchar(6) not null,
     [bar_date] date not null,
     [open] float not null,
@@ -41,13 +43,58 @@ BEGIN
 END
 go
 
-
-insert into stocks values('SZ', '000003')
-GO 
-
-insert into daily_bars values('NYSE', 'BIDU', '2019-12-10', 79.01, 77.23, 80.12, 75.12, 1378888.27);
+if not exists (select *
+from sys.tables
+where name='us_cal')
+BEGIN
+  create table us_cal
+  (
+    [trade_date] date not null,
+    [is_open] bit not null,
+    primary key (trade_date, is_open)
+  )
+END
 GO
 
-select * from stocks;
-select * from daily_bars;
+if not exists (select *
+from sys.tables
+where name='ch_cal')
+BEGIN
+  create table ch_cal
+  (
+    [trade_date] date not null,
+    [is_open] bit not null,
+    primary key (trade_date, is_open)
+  )
+END
+GO
+
+
+if not exists (select *
+from sys.tables
+where name='exchanges')
+BEGIN
+  create table exchanges
+  (
+    [ex_code] nvarchar(6) not null,
+    [country_code] NVARCHAR(3) not null,
+    primary key (ex_code, country_code)
+  )
+  insert into stocks
+  values('SZ', '000003')
+END
+GO
+
+insert into daily_bars
+values('NYSE', 'BIDU', '2019-12-10', 79.01, 77.23, 80.12, 75.12, 1378888.27);
+GO
+
+select *
+from stocks;
+select *
+from daily_bars;
+select *
+from us_cal;
+select *
+from ch_cal;
 GO
