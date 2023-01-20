1c423e90ceee03fe444eb65318bf24825f6ec118
Shibo Xing <shibox@amazon.com>
2023-01-20 04:05:53 +0000

    feat: add submodule branch

--- db_storage/download.sh
+++ db_storage/download.sh
@@ -2,16 +2,16 @@
 
 set -e
 
+git submodule foreach git pull
+
 # switch to storage place
 pushd db_storage/Storage
-git checkout -f detes
+git checkout -f main
 
 # recover docker tar filesystem
-git clone https://github.com/ShiboXing/Storage.git -b main --single-branch --depth 1 
-pushd Storage
+git clone https://github.com/ShiboXing/Storage.git -b detes --single-branch --depth 1 
 make
 ./deneutralizer
-popd
 
 # deploy container 
 docker_img=sql1:latest
