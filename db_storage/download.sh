43b342209d322601425c66d0336d0713d2fdc328
Shibo Xing <shibox@amazon.com>
2023-01-20 23:56:07 +0000

    refactor: sql credentials

--- db_storage/download.sh
+++ db_storage/download.sh
@@ -11,11 +11,16 @@ git checkout -f main
 # recover docker tar filesystem
 git clone https://github.com/ShiboXing/Storage.git -b detes --single-branch --depth 1 
 make
-./deneutralizer
+./deneutralizer Storage
 
 # deploy container 
-docker_img=sql1:latest
-docker import *.tar - ${docker_img}
+tag="latest"
+docker_img="sql1:${tag}"
+cat "Storage/${tag}.tar" | docker import - ${docker_img}
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" \
    -p 1433:1433 --name sql1 --hostname sql1 \
-   -d ${docker_img} sqlservr
+   -d ${docker_img} "/opt/mssql/bin/sqlservr"
+
+docker exec sql1 /opt/mssql-tools/bin/sqlcmd \
+-S localhost -U SA -P '<YourStrong@Passw0rd>' \
+-Q 'SELECT @@VERSION'
