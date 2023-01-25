21370af47f8388b7c8df5e122b7e0c2d6eb94624
Shibo Xing <shibox@amazon.com>
2023-01-25 04:53:57 +0000

    feat: sql funcs (trade_dates, delist)

--- rumble/detes/detes_helper/db/build_funcs.sql
+++ rumble/detes/detes_helper/db/build_funcs.sql
@@ -1,59 +1,56 @@
-create or alter proc get_all_last_dates
+create or alter view get_all_last_dates
 as
-    begin
-        select max(bar_date), l.code
-        from us_stock_list l left join us_daily_bars b
-            on l.code = b.code
-        where is_delisted = 0
-        group by l.code
-    end
-    return
+    select max(bar_date) last_date, l.code
+    from us_stock_list l left join us_daily_bars b
+        on l.code = b.code
+    where is_delisted = 0
+    group by l.code
 go
 
-create or alter proc get_last_trading_date
-    @region nvarchar(2)
-as 
+create or alter function get_last_trading_date (
+    @region nvarchar(2) = 'us'
+)
+    returns date
     begin
+        declare @max_date date
         if @region = 'cn'
         begin
-            select max(trade_date)
-            from cn_cal
-            where is_open=1
+            select @max_date = (
+                select max(trade_date)
+                from cn_cal
+                where is_open=1
+            )
         end
         else if @region = 'us'
         begin
-            select max(trade_date)
-            from us_cal
-            where is_open=1
+            select @max_date = (
+                select max(trade_date)
+                from us_cal
+                where is_open=1
+            )
         end
+        return @max_date
     end
-    return
 go
 
--- create or alter function delist_stocks()
--- returns @stocks_info table (bar_date date, code varchar(7), diff int)
---     begin
---         declare @last_date table (tmp_date date);
---         insert into @last_date 
---         select * from (exec get_last_trading_date 'us');
---         insert into @stocks_info
---         select max(bar_date), h.code, 
---         datediff(day, max(bar_date), (
---             select * from @last_date)
---         )
---         from us_stock_list h inner join us_daily_bars l
---         on h.code = l.code
---         where is_delisted = 0
---         group by h.code;
---         return
---     end
--- go
+create or alter function delist_stocks()
+returns @old_stocks table (last_date date, code VARCHAR(6), diff int)
+    begin
+        declare @last_date date;
+        set @last_date = dbo.get_last_trading_date('us')
 
+        insert into @old_stocks
+        select * from (
+            select *,
+                datediff(day, last_date, @last_date) diff_days
+            from get_all_last_dates
+        ) tmp
+        where tmp.diff_days > 14
 
--- select * from dbo.delist_stocks();
--- declare @tmp_stocks table (d date, c VARCHAR(7), i int);
--- select * from delist_stocks;
--- select code from us_stock_list uss inner join
---     @tmp_stocks ts on ts.c = uss.code
--- where i > 7;
-    
+        return;
+    end
+go
+
+-- select dbo.get_last_trading_date('us')
+-- select * from dbo.delist_stocks()
+-- select * from get_all_last_dates
