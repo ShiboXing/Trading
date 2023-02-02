ac8d12ffde279a4b9976622ba126ce9faece5778
Shibo Xing <shibox@amazon.com>
2023-02-02 03:47:05 +0000

    feat: add odds calculation in extended db_helper

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from rumble.rumble.tech.domains import Domains
 from sota.computer_vision.models.vgg import VGG
 
 import torch.nn.modules.module
 
 if __name__ == "__main__":
+    d = Domains()
+    print(d.get_streak_odds())
     vgg = VGG(input_c=1).cuda()
     test = torch.rand((4, 1, 40, 200), dtype=torch.float).cuda()
     res = vgg(test)
