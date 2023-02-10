6cf9ea622a952d1f43e903fc55ca91f58fdb9ced
Shibo Xing <shibox@amazon.com>
2023-02-10 03:39:52 +0000

    feat: calculate vol returns

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import torch.nn.modules.module
 
 if __name__ == "__main__":
     d = Domains()
-    rets = d.get_agg_rets("2023-01-10", "Technology", "sector")
-    rets = d.get_index_rets("2023-01-03", "2023-02-03")
+    agg_rets = d.get_agg_rets("2023-01-10", "Technology", "sector")
+    index_rets = d.get_index_rets("2023-01-03", "2023-02-03")
 
     vgg = VGG(input_c=1).cuda()
     test = torch.rand((4, 1, 32, 200), dtype=torch.float).cuda()
