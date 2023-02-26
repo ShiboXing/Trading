529d5a8e03f5f65e465d92687decf96d70372bc1
Ubuntu <ubuntu@ip-172-31-8-45.us-west-2.compute.internal>
2023-02-26 19:41:12 +0000

    feat: init_install boost

--- init_install.sh
+++ init_install.sh
@@ -7,17 +7,18 @@ sudo apt-get install build-essential \
     autotools-dev \
     libcu-dev \
     libbz2-d2v \
-    libboost-all-dev \
-    devtoolset-10
+    libboost-all-dev 
 
 # install c++ boost library
-wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz \
-    && tar xvf boost_1_81_0.tar.gz \
-    && pushd boost_1_81_0 \
-    && ./bootstrap.sh --prefix=/usr/ \
-    && sudo ./b2 install \
-    && popd \
-    && rm -rf boost_1_81_0.tar.gz boost_1_81_0
+if [ ! -d "/usr/include/boost" ] then
+    wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz \
+        && tar xvf boost_1_81_0.tar.gz \
+        && pushd boost_1_81_0 \
+        && ./bootstrap.sh --prefix=/usr/ \
+        && sudo ./b2 install \
+        && popd \
+        && sudo rm -rf boost_1_81_0.tar.gz boost_1_81_0
+fi
     
 # install python packages
 mamba install ipdb \
