8e6941f840538aff2ea7818f5938bfcfe1c28a52
Shibo Xing <shx26@pitt.edu>
2023-02-27 03:43:54 +0000

    feat: add mssql driver

--- init_install.sh
+++ init_install.sh
@@ -2,24 +2,32 @@
 
 set -e 
 
-# ADD GPG REPO KEYS
+### ADD GPG REPO KEYS ###
+
 # nvidia
 sudo apt-key del 7fa2af80
 source /etc/os-release
-distro=$ID`echo $VERSION_ID | sed 's/\.//'`; arch=$(uname -m); curl -o nvidia-keyring.deb -L https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+distro=$ID`lsb_release -rs | sed 's/\.//'`; arch=$(uname -m); curl -o nvidia-keyring.deb -L https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i nvidia-keyring.deb
 rm nvidia-keyring.deb
+
 # docker
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# msft odbc
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/mssql.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/mssql.gpg] https://packages.microsoft.com/ubuntu/22.04/prod \
+    $(lsb_release -cs) main" \ | sudo tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
 
 # install ubuntu packages
 sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install -y build-essential \
+sudo ACCEPT_EULA=Y apt-get install -y build-essential \
     g++ \
     python3-dev \
     autotools-dev \
@@ -32,23 +40,13 @@ sudo apt-get install -y build-essential \
     containerd.io \
     docker-buildx-plugin \
     docker-compose-plugin \
+    msodbcsql18 \
+    mssql-tools18 \
     nvidia-driver-525 \
     cuda-drivers-fabricmanager-525
 
-# start servives
-if [ -z $(ps -e | grep dockerd)]
-then
-    sudo systemctl stop docker
-    rm -f /var/run/docker.pid
-    sudo groupadd docker | true
-    newgrp docker
-    sudo dockerd & # run docker daemon
-    sudo chmod 666 /var/run/docker.sock 
-    docker images
-fi
-
 # install cuda toolkit
-if [ -z $(apt list --installed | grep cuda) ]
+if [ -z "$(apt list --installed | grep cuda)" ];
 then
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
     sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -82,14 +80,16 @@ then
 fi
 
 # install python packages
-mamba install -y ipdb \
+mamba install -y \
+    python=3.10 \
+    ipdb \
     black \
     yfinance \
     numpy \
     pandas \
     matplotlib \
     sqlalchemy \
-    python=3.10 \
+    pyodbc \
     pytorch \
     pytorch-cuda=11.7 \
     -c pytorch \
@@ -100,4 +100,21 @@ pip install jupyterlab notebook yahooquery tushare
 rm -rf build/ rumble.egg-info
 pip install .
 
+# start servives
+if [ -z "$(ps -e | grep dockerd)" ];
+then
+    sudo systemctl stop docker
+    rm -f /var/run/docker.pid
+    sudo groupadd docker | true
+    newgrp docker
+    sudo dockerd & # run docker daemon
+    sudo chmod 666 /var/run/docker.sock 
+    docker images
+fi
 
+# set up odbc driver
+if [[ ! $PATH == *"/opt/mssql-tools18/bin"* ]];
+then
+    echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+    source ~/.bashrc
+fi
