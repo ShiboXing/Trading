0bc360fa17fe02933993f4b5a1a7325b749daf3e
Shibo Xing <shibox@amazon.com>
2023-03-28 21:59:47 +0000

    feat: revise cuda installation

--- init_install.sh
+++ init_install.sh
@@ -5,11 +5,11 @@ set -e
 ### ADD GPG REPO KEYS ###
 
 # nvidia
-sudo apt-key del 7fa2af80
-source /etc/os-release
-distro=$ID`lsb_release -rs | sed 's/\.//'`; arch=$(uname -m); curl -o nvidia-keyring.deb -L https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i nvidia-keyring.deb
-rm nvidia-keyring.deb
+# sudo apt-key del 7fa2af80
+# source /etc/os-release
+# distro=$ID`lsb_release -rs | sed 's/\.//'`; arch=$(uname -m); curl -o nvidia-keyring.deb -L https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+# sudo dpkg -i nvidia-keyring.deb
+# rm nvidia-keyring.deb
 
 # docker
 sudo mkdir -m 0755 -p /etc/apt/keyrings
@@ -23,6 +23,8 @@ sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --batch -
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/mssql.gpg] https://packages.microsoft.com/ubuntu/22.04/prod \
     $(lsb_release -cs) main" \ | sudo tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
+
+#########################
 
 # install ubuntu packages
 sudo apt-get update
@@ -41,21 +43,7 @@ sudo ACCEPT_EULA=Y apt-get install -y build-essential \
     docker-buildx-plugin \
     docker-compose-plugin \
     msodbcsql18 \
-    mssql-tools18 \
-    nvidia-driver-525 \
-    cuda-drivers-fabricmanager-525
-
-# install cuda toolkit
-if [ -z "$(apt list --installed | grep cuda)" ];
-then
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-    sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget https://developer.download.nvidia.com/compute/cuda/12.0.1/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.1-525.85.12-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.1-525.85.12-1_amd64.deb
-    sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
-    sudo apt-get update
-    sudo apt-get install -y cuda
-fi
+    mssql-tools18 
 
 # install c++ boost library
 if [ ! -d "/usr/include/boost" ]
@@ -68,6 +56,15 @@ then
         && popd \
         && sudo rm -rf boost_1_81_0.tar.gz boost_1_81_0
 fi
+
+# reinstall cuda driver and toolkit
+# sudo /usr/local/cuda-11.8/bin/cuda-uninstaller | true
+sudo apt-get autoremove
+sudo apt-get purge -y *nvidia* *cuda*
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+sudo sh cuda_11.8.0_520.61.05_linux.run --silent
+rm cuda_11.8.0_520.61.05_linux.run
+export CUDA_HOME=/usr/local/cuda
     
 # install mamba
 if [ ! -d "/home/ubuntu/mambaforge" ]
@@ -91,24 +88,20 @@ mamba install -y \
     sqlalchemy \
     pyodbc \
     pytorch \
-    pytorch-cuda=11.7 \
+    pytorch-cuda=11.8 \
     -c pytorch \
     -c nvidia
 pip install jupyterlab notebook yahooquery tushare
 
-# install rumble
-rm -rf build/ rumble.egg-info
-pip install .
-
 # start servives
 if [ -z "$(ps -e | grep dockerd)" ];
 then
-    sudo systemctl stop docker
-    rm -f /var/run/docker.pid
-    sudo groupadd docker | true
-    newgrp docker
-    sudo dockerd & # run docker daemon
+    sudo pkill -9 dockerd
+    sudo rm -f /var/run/docker.pid
     sudo chmod 666 /var/run/docker.sock 
+    sudo dockerd & > /dev/null # run docker daemon
+    sleep 5
+    echo "=================== docker service initialization completed ==================="
     docker images
 fi
 
@@ -118,3 +111,7 @@ then
     echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
     source ~/.bashrc
 fi
+
+# install rumble
+rm -rf build/ rumble.egg-info
+pip install .
