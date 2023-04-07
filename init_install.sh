2c608ced74c7dbb09127f35a92102552f1aff5d8
Shibo Xing <shibox@amazon.com>
2023-04-07 03:23:45 +0000

    build: install nvidia-docker, improve nvidia driver installation

--- init_install.sh
+++ init_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e 
-
 # install fundamental apt packages
+sudo apt-get update -y 
 sudo apt-get install -y build-essential \
     g++ \
     python3-dev \
@@ -20,6 +19,13 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 rm cuda-keyring_1.0-1_all.deb*
 
+# nvidia docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    && curl -fsL https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
 # docker
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -36,15 +42,9 @@ echo \
 sudo apt-get update
 #########################
 
-# install ubuntu packages
+# install other ubuntu packages
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo ACCEPT_EULA=Y apt-get install -y build-essential \
-    g++ \
-    python3-dev \
-    autotools-dev \
-    libboost-all-dev \
-    curl \
-    ca-certificates \
+sudo ACCEPT_EULA=Y apt-get install -y \
     gnupg \
     lsb-release \
     docker-ce \
@@ -52,34 +52,23 @@ sudo ACCEPT_EULA=Y apt-get install -y build-essential \
     containerd.io \
     docker-buildx-plugin \
     docker-compose-plugin \
+    nvidia-container-toolkit \
     msodbcsql18 \
     mssql-tools18 
 
-# install c++ boost library
-if [ ! -d "/usr/include/boost" ]
-then
-    wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz \
-        && tar xvf boost_1_81_0.tar.gz \
-        && pushd boost_1_81_0 \
-        && ./bootstrap.sh --prefix=/usr/ \
-        && sudo ./b2 install \
-        && popd \
-        && sudo rm -rf boost_1_81_0.tar.gz boost_1_81_0
-fi
-
 # install nvidia driver
 if ! nvidia-smi &> /dev/null; then
-    sudo apt-get autoremove
+    sudo apt-get autoremove -y
     sudo apt-get purge -y *nvidia* *cuda*
-    distribution=$(. /etc/os-release; echo $ID$VERSION_ID | sed -e 's/\.//g')
     sudo apt-get install -y linux-headers-$(uname -r)
     sudo apt-get install -y nvidia-driver-525
+    # need reboot
 fi
 
 # install mamba
 if [ ! -d "/home/ubuntu/mambaforge" ]
 then
-    curl -L -o mambaforge.sh https://github.com/conda-forge/miniforge/releases/download/22.11.1-4/Mambaforge-22.11.1-4-Linux-x86_64.sh \
+    curl -L -o mambaforge.sh https://github.com/conda-forge/miniforge/releases/download/23.1.0-1/Mambaforge-23.1.0-1-Linux-x86_64.sh \
         && bash mambaforge.sh -b -u \
         && rm mambaforge.sh \
         && /home/ubuntu/mambaforge/bin/mamba init \
@@ -105,16 +94,14 @@ mamba install -y \
 pip install jupyterlab notebook yahooquery tushare
 
 # start servives
-if [ -z "$(ps -e | grep dockerd)" ];
-then
-    sudo pkill -9 dockerd
-    sudo rm -f /var/run/docker.pid
-    sudo chmod 666 /var/run/docker.sock 
-    sudo dockerd & > /dev/null # run docker daemon
-    sleep 5
-    echo "=================== docker service initialization completed ==================="
-    docker images
-fi
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo pkill -9 dockerd
+sudo rm -f /var/run/docker.pid
+sudo dockerd & > /dev/null # run docker daemon
+sleep 5
+sudo chmod 666 /var/run/docker.sock
+echo "=================== docker service initialization completed ==================="
+docker images
 
 # set up odbc driver
 if [[ ! $PATH == *"/opt/mssql-tools18/bin"* ]];
