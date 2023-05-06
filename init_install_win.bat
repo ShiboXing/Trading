a771693e65dc99a19f2f76c430a96398f3432c98
Shibo Xing <stanleyhsing26@gmail.com>
2023-05-06 11:57:34 -0400

    build: add some batch for gcc/boost on STUPID windows

--- init_install_win.bat
+++ init_install_win.bat
@@ -1,3 +1,13 @@
+@REM install gcc and boost
+@REM where g++ > nul 2>&1
+@REM if %errorlevel% neq 0 (
+@REM     echo g++ is not installed, installing MSYS2...
+@REM     curl https://github.com/msys2/msys2-installer/releases/download/2023-03-18/msys2-x86_64-20230318.exe -o msy32_installer.exe
+@REM     msy32_installer.exe
+@REM     curl https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.zip -o boost_installer.zip
+@REM )
+
+
 
 @REM install python packages
 pip install ipdb ^
@@ -20,9 +30,11 @@ pip3 install torch torchvision torchaudio --index-url https://download.pytorch.o
 @REM install odbc driver
 reg query "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver 18 for SQL Server" > nul
 if %errorlevel% neq 0 (
-    curl -L https://go.microsoft.com/fwlink/?linkid=2223270 -o odbc.msi
+    curl https://go.microsoft.com/fwlink/?linkid=2223270 -o odbc.msi
     ".\odbc.msi"
     del ".\odbc.msi"
 )
 
-@REM install nvidia driver
+@REM install rumble
+rm -rf build/ rumble.egg-info
+pip install .
