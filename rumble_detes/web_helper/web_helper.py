db76a193ec1402da00977661531f5ebdba3466ea
Shibo Xing <shx26@pitt.edu>
2024-04-25 23:37:38 +0800

    feat: download s&p list through requests

--- rumble_detes/web_helper/web_helper.py
+++ rumble_detes/web_helper/web_helper.py
@@ -1,6 +1,9 @@
 import yfinance as yf
+import requests
+from lxml import html
 
 class web_helper:
     def __init__(self):
         pass
-    
+
+res = requests.get("https://www.slickcharts.com/sp500")
